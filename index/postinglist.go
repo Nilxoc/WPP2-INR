751db0acfece67e384c1bea71f37583e0e7e7cde
Colin Gebler <colin.gebler@gebler-online.de>
2021-05-05 12:41:10 +0200

    dsfsdfdsaf

--- index/postinglist.go
+++ index/postinglist.go
@@ -1,12 +1,26 @@
 package index
 
-type PostingListEntry struct {
+type Posting struct {
 	DocID int64
-	Pos   int64
+	Pos   []int64
 }
 
-type PostingList []PostingListEntry
+type PostingList []Posting
 
 func (pl *PostingList) String() string {
 	return "TODO"
+}
+
+func (pl *PostingList) Intersect(other *PostingList) PostingList {
+	answer := make(PostingList, 0)
+	var p1, p2 int
+	for p1 < len(*pl) && p2 < len(*other) {
+		if (*pl)[p1].DocID == (*other)[p2].DocID {
+			answer = append(answer, (*pl)[p1])
+			p1 += 1
+			p2 += 1
+		}
+
+	}
+	return answer
 }
