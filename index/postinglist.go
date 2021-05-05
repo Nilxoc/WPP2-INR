e5b4f524bae1594ffda5ee8b9a6f2e3bde746b89
Colin Gebler <colin.gebler@gebler-online.de>
2021-05-05 21:39:01 +0200

    Phrase Queries

--- index/postinglist.go
+++ index/postinglist.go
@@ -19,6 +19,23 @@ func (p *Posting) Merge(other *Posting) Posting {
 
 type PostingList []Posting
 
+func intersectArrays(a []int64, b []int64) []int64 {
+	var i, j int
+	res := make([]int64, 0)
+	for i < len(a) && j < len(b) {
+		if a[i] == b[j] {
+			res = append(res, a[i])
+			i += 1
+			j += 1
+		} else if a[i] < b[j] {
+			i += 1
+		} else {
+			j += 1
+		}
+	}
+	return res
+}
+
 func (pl *PostingList) String() string {
 	return "TODO"
 }
@@ -91,7 +108,7 @@ func (pl *PostingList) positionalIntersect(other *PostingList, k int64, kCond fu
 					pp2 += 1
 				}
 
-				for len(l) > 0 && int64(math.Abs(float64(l[0])-float64(pos1[pp1]))) > k {
+				for len(l) > 0 && !kCond(pos1[pp1], l[0], k) {
 					if len(l) > 1 {
 						l = l[1:]
 					} else {
@@ -128,4 +145,44 @@ func (pl *PostingList) PositionalIntersect(other *PostingList, k int64) PostingL
 	return pl.positionalIntersect(other, k, func(num1, num2, k int64) bool {
 		return int64(math.Abs(float64(num1)-float64(num2))) <= k
 	})
+}
+
+func (pl *PostingList) PhraseIntersect(others []*PostingList) PostingList {
+	currPl := *pl
+	var k int
+	for i, v := range others {
+		res := currPl.positionalIntersect(v, int64(i+1), func(num1, num2, k int64) bool { return num2-num1 == k })
+		for _, v2 := range res {
+			for k < len(currPl) && currPl[k].DocID < v2.DocID {
+				currPl = append(currPl[:k], currPl[k+1:]...)
+			}
+
+			if k >= len(currPl) {
+				k -= 1
+				break
+			}
+
+			if v2.DocID == currPl[k].DocID {
+				currPl[k] = Posting{v2.DocID, intersectArrays(currPl[k].Pos, v2.Pos)}
+				k += 1
+			}
+		}
+		currPl = currPl[:k]
+		k = 0
+	}
+
+	//Add Positions after first to Position List. This is only important for proximity queries
+	//Even then, the middle values could technically be omitted
+	res := make(PostingList, 0, len(currPl))
+	for _, v := range currPl {
+		var posList []int64
+		for _, pos := range v.Pos {
+			posList = append(posList, pos)
+			for i := int64(0); i < int64(len(others)); i++ {
+				posList = append(posList, pos+int64(1)+i)
+			}
+		}
+		res = append(res, Posting{v.DocID, posList})
+	}
+	return res
 }
