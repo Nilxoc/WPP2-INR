b8c809ae2fc64ee915638397435b22873b2ed082
Colin Gebler <colin.gebler@gebler-online.de>
2021-05-05 15:00:08 +0200

    PostionalIntersect

--- index/index_test.go
+++ index/index_test.go
@@ -1,6 +1,8 @@
 package index
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIntersect(t *testing.T) {
 	var list1 = make(PostingList, 0)
@@ -15,6 +17,11 @@ func TestIntersect(t *testing.T) {
 	list2 = append(list2, Posting{8, []int64{25, 39, 90}})
 
 	var res = list1.Intersect(&list2)
+
+	if len(res) != 2 {
+		t.Errorf("wrong posting count, expected 2, got %d", len(res))
+	}
+
 	if res[0].DocID != 1 {
 		t.Errorf("First DocID Wrong, Expected 1, got %d", res[0].DocID)
 	}
@@ -55,5 +62,135 @@ func TestIntersect(t *testing.T) {
 
 	if !arrayEqual {
 		t.Errorf("Wrong Positions in first second, expected [1,2,5,6,7,8],got %v", res[1].Pos)
+	}
+}
+
+func TestUnion(t *testing.T) {
+	var list1 = make(PostingList, 0)
+	var list2 = make(PostingList, 0)
+
+	list1 = append(list1, Posting{1, []int64{2, 5, 8}})
+	list1 = append(list1, Posting{5, []int64{6, 9, 20}})
+	list1 = append(list1, Posting{8, []int64{3, 8, 89}})
+
+	list2 = append(list2, Posting{1, []int64{1, 6, 7}})
+	list2 = append(list2, Posting{4, []int64{8, 23, 91}})
+	list2 = append(list2, Posting{8, []int64{25, 39, 90}})
+
+	var res = list1.Union(&list2)
+
+	if len(res) != 4 {
+		t.Errorf("wrong posting count, expected 4, got %d", len(res))
+	}
+
+	if res[0].DocID != 1 {
+		t.Errorf("First DocID Wrong, Expected 1, got %d", res[0].DocID)
+	}
+
+	var arrayEqual = true
+	var correct = []int64{1, 2, 5, 6, 7, 8}
+
+	if len(res[0].Pos) != len(correct) {
+		arrayEqual = false
+	} else {
+		for i, v := range res[0].Pos {
+			if v != correct[i] {
+				arrayEqual = false
+			}
+		}
+	}
+
+	if !arrayEqual {
+		t.Errorf("wrong positions in first posting, expected [1,2,5,6,7,8],got %v", res[0].Pos)
+	}
+
+	if res[1].DocID != 4 {
+		t.Errorf("second docid Wrong, expected 4, got %d", res[1].DocID)
+	}
+
+	if res[2].DocID != 5 {
+		t.Errorf("third docid Wrong, expected 5, got %d", res[2].DocID)
+	}
+
+	if res[3].DocID != 8 {
+		t.Errorf("fourth docid Wrong, expected 8, got %d", res[1].DocID)
+	}
+
+	arrayEqual = true
+	correct = []int64{3, 8, 25, 39, 89, 90}
+
+	if len(res[3].Pos) != len(correct) {
+		arrayEqual = false
+	} else {
+		for i, v := range res[3].Pos {
+			if v != correct[i] {
+				arrayEqual = false
+			}
+		}
+	}
+
+	if !arrayEqual {
+		t.Errorf("wrong positions in fourth posting, expected [1,2,5,6,7,8],got %v", res[3].Pos)
+	}
+}
+
+func TestPostitionalIntersect(t *testing.T) {
+	var list1 = make(PostingList, 0)
+	var list2 = make(PostingList, 0)
+
+	list1 = append(list1, Posting{1, []int64{2, 5, 8}})
+	list1 = append(list1, Posting{5, []int64{6, 9, 20}})
+	list1 = append(list1, Posting{8, []int64{3, 8, 89}})
+
+	list2 = append(list2, Posting{1, []int64{1, 6, 7}})
+	list2 = append(list2, Posting{4, []int64{8, 23, 91}})
+	list2 = append(list2, Posting{8, []int64{25, 39, 90}})
+
+	var res = list1.PositionalIntersect(&list2, 1)
+
+	if len(res) != 2 {
+		t.Errorf("wrong posting count, expected 2, got %d", len(res))
+	}
+
+	if res[0].DocID != 1 {
+		t.Errorf("First DocID Wrong, Expected 1, got %d", res[0].DocID)
+	}
+
+	var arrayEqual = true
+	var correct = []int64{1, 2, 5, 6, 7, 8}
+
+	if len(res[0].Pos) != len(correct) {
+		arrayEqual = false
+	} else {
+		for i, v := range res[0].Pos {
+			if v != correct[i] {
+				arrayEqual = false
+			}
+		}
+	}
+
+	if !arrayEqual {
+		t.Errorf("wrong positions in first posting, expected [1,2,5,6,7,8],got %v", res[0].Pos)
+	}
+
+	if res[1].DocID != 8 {
+		t.Errorf("second docid Wrong, expected 8, got %d", res[1].DocID)
+	}
+
+	arrayEqual = true
+	correct = []int64{89, 90}
+
+	if len(res[1].Pos) != len(correct) {
+		arrayEqual = false
+	} else {
+		for i, v := range res[1].Pos {
+			if v != correct[i] {
+				arrayEqual = false
+			}
+		}
+	}
+
+	if !arrayEqual {
+		t.Errorf("wrong positions in second posting, expected [1,2,5,6,7,8],got %v", res[1].Pos)
 	}
 }
--- index/postinglist.go
+++ index/postinglist.go
@@ -1,10 +1,20 @@
 package index
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 type Posting struct {
 	DocID int64
 	Pos   []int64
+}
+
+func (p *Posting) Merge(other *Posting) Posting {
+	newPos := append(p.Pos, other.Pos...)
+	sort.Slice(newPos,
+		func(p, q int) bool { return newPos[p] < newPos[q] })
+	return Posting{p.DocID, newPos}
 }
 
 type PostingList []Posting
@@ -18,9 +28,7 @@ func (pl *PostingList) Intersect(other *PostingList) PostingList {
 	var p1, p2 int
 	for p1 < len(*pl) && p2 < len(*other) {
 		if (*pl)[p1].DocID == (*other)[p2].DocID {
-			newPos := append((*pl)[p1].Pos, (*other)[p2].Pos...)
-			sort.Slice(newPos, func(p, q int) bool { return newPos[p] < newPos[q] })
-			answer = append(answer, Posting{(*pl)[p1].DocID, newPos})
+			answer = append(answer, (*pl)[p1].Merge(&(*other)[p2]))
 			p1 += 1
 			p2 += 1
 		} else if (*pl)[p1].DocID < (*other)[p2].DocID {
@@ -30,4 +38,86 @@ func (pl *PostingList) Intersect(other *PostingList) PostingList {
 		}
 	}
 	return answer
+}
+
+func (pl *PostingList) Union(other *PostingList) PostingList {
+	answer := make(PostingList, 0)
+	var p1, p2 int
+	for p1 < len(*pl) && p2 < len(*other) {
+		if (*pl)[p1].DocID == (*other)[p2].DocID {
+			answer = append(answer, (*pl)[p1].Merge(&(*other)[p2]))
+			p1 += 1
+			p2 += 1
+		} else if (*pl)[p1].DocID < (*other)[p2].DocID {
+			answer = append(answer, (*pl)[p1])
+			p1 += 1
+		} else {
+			answer = append(answer, (*other)[p2])
+			p2 += 1
+		}
+	}
+
+	for p1 < len(*pl) {
+		answer = append(answer, (*pl)[p1])
+		p1 += 1
+	}
+
+	for p2 < len(*other) {
+		answer = append(answer, (*other)[p2])
+		p1 += 1
+	}
+	return answer
+}
+
+func (pl *PostingList) positionalIntersect(other *PostingList, k int64, kCond func(num1, num2, k int64) bool) PostingList {
+	answer := make(PostingList, 0)
+	var p1, p2 int
+	for p1 < len(*pl) && p2 < len(*other) {
+		if (*pl)[p1].DocID == (*other)[p2].DocID {
+			l := make([]int64, 0)
+			outPos := make([]int64, 0)
+			var pos1 = (*pl)[p1].Pos
+			var pos2 = (*other)[p2].Pos
+			var pp1, pp2 int
+			for pp1 < len(pos1) {
+				for pp2 < len(pos2) {
+					if kCond(pos1[pp1], pos2[pp2], k) {
+						l = append(l, pos2[pp2])
+					} else if pos2[pp2] > pos1[pp1] {
+						break
+					}
+					pp2 += 1
+				}
+
+				for len(l) > 0 && int64(math.Abs(float64(l[0])-float64(pos1[pp1]))) > k {
+					if len(l) > 1 {
+						l = l[1:]
+					} else {
+						l = []int64{}
+					}
+				}
+
+				if len(l) > 0 { //Geht das schneller?
+					outPos = append(outPos, l...)
+					outPos = append(outPos, pos1[pp1])
+				}
+				pp1 += 1
+			}
+			sort.Slice(outPos, func(p, q int) bool { return outPos[p] < outPos[q] })
+			answer = append(answer, Posting{(*pl)[p1].DocID, outPos})
+			p1 += 1
+			p2 += 1
+		} else if (*pl)[p1].DocID < (*other)[p2].DocID {
+			p1 += 1
+		} else {
+			p2 += 1
+		}
+	}
+	return answer
+}
+
+func (pl *PostingList) PositionalIntersect(other *PostingList, k int64) PostingList {
+	return pl.positionalIntersect(other, k, func(num1, num2, k int64) bool {
+		return int64(math.Abs(float64(num1)-float64(num2))) <= k
+	})
 }
