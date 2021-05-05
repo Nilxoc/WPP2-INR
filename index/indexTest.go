820de817b8991e1cd07b74c9955b43e4957231e0
Colin Gebler <colin.gebler@gebler-online.de>
2021-05-05 13:23:01 +0200

    Intersect Test

---
+++ index/indexTest.go
@@ -0,0 +1,42 @@
+package index
+
+import "testing"
+
+func TestIntersect(t *testing.T) {
+	var list1 = make(PostingList, 0)
+	var list2 = make(PostingList, 1)
+
+	list1 = append(list1, Posting{1, []int64{2, 5, 8}})
+	list1 = append(list1, Posting{5, []int64{6, 9, 20}})
+	list1 = append(list1, Posting{8, []int64{3, 8, 89}})
+
+	list2 = append(list2, Posting{1, []int64{1, 6, 7}})
+	list2 = append(list2, Posting{4, []int64{8, 213, 9}})
+	list2 = append(list2, Posting{8, []int64{25, 39, 89}})
+
+	var res = list1.Intersect(&list2)
+	if res[0].DocID != 1 {
+		t.Errorf("First DocID Wrong, Expected 1, got %d", res[0].DocID)
+	}
+
+	var arrayEqual = true
+	var correct = []int64{1, 2, 5, 6, 7, 8}
+
+	if len(res[0].Pos) != len(correct) {
+		arrayEqual = false
+	} else {
+		for i, v := range res[0].Pos {
+			if v != correct[i] {
+				arrayEqual = false
+			}
+		}
+	}
+
+	if !arrayEqual {
+		t.Errorf("Wrong Positions in first posting, expected {1,2,5,6,7,8}")
+	}
+
+	if res[1].DocID != 8 {
+		t.Errorf("Second DocID Wrong, Expected 8, got %d", res[1].DocID)
+	}
+}
--- index/postinglist.go
+++ index/postinglist.go
@@ -1,5 +1,7 @@
 package index
 
+import "sort"
+
 type Posting struct {
 	DocID int64
 	Pos   []int64
@@ -16,11 +18,16 @@ func (pl *PostingList) Intersect(other *PostingList) PostingList {
 	var p1, p2 int
 	for p1 < len(*pl) && p2 < len(*other) {
 		if (*pl)[p1].DocID == (*other)[p2].DocID {
-			answer = append(answer, (*pl)[p1])
+			newPos := append((*pl)[p1].Pos, (*other)[p2].Pos...)
+			sort.Slice(newPos, func(p, q int) bool { return newPos[p] < newPos[q] })
+			answer = append(answer, Posting{(*pl)[p1].DocID, newPos})
 			p1 += 1
 			p2 += 1
+		} else if (*pl)[p1].DocID < (*other)[p2].DocID {
+			p1 += 1
+		} else {
+			p2 += 1
 		}
-
 	}
 	return answer
 }
