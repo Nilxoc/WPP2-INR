40e7cd01770b97c632cc422bdaa14a435a96be42
Glains <lukas-kannenberg@web.de>
2021-05-09 22:19:12 +0200

    refactor: rename func to #testCompile

--- query/ast_test.go
+++ query/ast_test.go
@@ -9,34 +9,34 @@ import (
 // validates if sample queries can be compiled
 
 func TestParseSingle(t *testing.T) {
-	parseNoError(t, "test1")
+	testCompile(t, "test1")
 }
 
 func TestParseAnd(t *testing.T) {
-	parseNoError(t, "test1 AND test2")
+	testCompile(t, "test1 AND test2")
 }
 
 func TestParseProximity(t *testing.T) {
-	parseNoError(t, "term4 /2 term5")
+	testCompile(t, "term4 /2 term5")
 }
 
 func TestParsePhrase(t *testing.T) {
-	parseNoError(t, `term1 term2 term3`)
+	testCompile(t, `term1 term2 term3`)
 }
 
 func TestParsePhraseNested(t *testing.T) {
-	parseNoError(t, `term1 AND "term2 term3"`)
+	testCompile(t, `term1 AND "term2 term3"`)
 }
 
 func TestParseBrackets(t *testing.T) {
-	parseNoError(t, "( term1 AND term2 ) AND term3")
+	testCompile(t, "( term1 AND term2 ) AND term3")
 }
 
 func TestParseFull(t *testing.T) {
-	parseNoError(t, `("term1 term2" OR term3) AND NOT term4 /3 term5`)
+	testCompile(t, `("term1 term2" OR term3) AND NOT term4 /3 term5`)
 }
 
-func parseNoError(t *testing.T, q string) {
+func testCompile(t *testing.T, q string) {
 	cfg := &config.Config{}
 	idx := index.NewIndexEmpty(cfg)
 	ctx := Context{
