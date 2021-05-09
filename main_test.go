020272a702a6a9e59a1b3175eac2d6f485d749f6
Colin Gebler <colin.gebler@gebler-online.de>
2021-05-09 22:07:16 +0200

    test: Exercise examples (part 1)

--- main_test.go
+++ main_test.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"g1.wpp2.hsnr/inr/boolret/config"
 	"path"
 	"runtime"
 	"testing"
 	"time"
+
+	"g1.wpp2.hsnr/inr/boolret/config"
+	"g1.wpp2.hsnr/inr/boolret/query"
 
 	"g1.wpp2.hsnr/inr/boolret/file"
 	"g1.wpp2.hsnr/inr/boolret/index"
 	"g1.wpp2.hsnr/inr/boolret/tokenizer"
 )
 
-func TestMain(t *testing.T) {
-
+func constructIndexForTest(t *testing.T) *index.Index {
+	//TODO Sollte wahrscheinlich error verwenden wenn doc_dump nicht
+	//gefunden
 	var docSource string
 
 	_, testFnPath, _, _ := runtime.Caller(0)
@@ -21,7 +24,7 @@ func TestMain(t *testing.T) {
 	docSource, err := file.AbsPath(path.Join(workDir, "doc_dump.txt"))
 	if err != nil {
 		t.Errorf("Could not get Abs Path to doc_dump.txt: %v", err)
-		return
+		return nil
 	}
 
 	cfg := config.Config{
@@ -41,10 +44,18 @@ func TestMain(t *testing.T) {
 
 	if err := tokenizer.ParseSingleFile(docSource); err != nil {
 		t.Logf("No doc_dump.txt file found. Skipping Performance-Testing")
-		return
+		return nil
 	}
 	t.Logf("Loaded and parsed document within %s", time.Since(tokStart).String())
 
+	return indexInstance
+}
+
+func TestMain(t *testing.T) {
+	indexInstance := constructIndexForTest(t)
+	if indexInstance == nil {
+		return
+	}
 	token1 := "fresher"
 	tStart := time.Now()
 	res := indexInstance.GetTerm(token1)
@@ -68,4 +79,30 @@ func TestMain(t *testing.T) {
 	}
 	t.Logf("Found corrected Terms within %s", time.Since(rStart).String())
 
+}
+
+func tryQuery(query string, t *testing.T, parser *query.AstQueryParser) {
+	q, _ := parser.Parse(query)
+	_, _ = q.Evaluate()
+}
+
+func TestExamples(t *testing.T) {
+	indexInstance := constructIndexForTest(t)
+
+	cfg, _ := config.Parse()
+
+	ctx := query.Context{
+		Index:  indexInstance,
+		Config: cfg,
+	}
+
+	parser := query.AstQueryParser{Context: &ctx}
+
+	//TODO performance
+	tryQuery("blood AND pressure", t, &parser)
+	tryQuery("blood AND NOT pressure", t, &parser)
+	tryQuery("(blood OR pressure) AND cardiovascular", t, &parser)
+	tryQuery("\"blood pressure\"", t, &parser)
+	tryQuery("diet \\10 health", t, &parser)
+	tryQuery("diet \\10 health AND \"red wine\"", t, &parser)
 }
